6412b3a2e9c5233334457397ae89fc9a72b41e2c
Khaletskiy-Timofey <haltimastro@gmail.com>
2025-04-07 17:37:57 +0300

    Use struction objects

--- MainProject/Student.h
+++ MainProject/Student.h
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Student
 {
-	public:
-		string name;
-		int age;
-		double avgMark;
-		bool isAlive;
+public:
+	string name;
+	int age;
+	double avgMark;
+	bool isAlive;
+
+	string showStudentInfo()
+	{
+		string info = "";
+
+		info += "Name: " + name;
+		info += ", Age: " + to_string(age);
+		info += ", Average mark: " + to_string(avgMark);
+		info += ", Is alive: ";
+		info += (isAlive ? "Yes" : "No");
+
+		return info;
+	}
 };
--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -1,23 +1,34 @@
 #include "Student.h"
 
+void change(Student student)
+{
+	student.name = "Vova";
+	student.age = 16;
+	student.avgMark = 10.0;
+	student.isAlive = false;
+}
+
 int main()
 {
-	Student st1, st2;
+	Student st1, temp;
 
 	st1.name = "Bogdan";
 	st1.age = 14;
 	st1.avgMark = 9.5;
 	st1.isAlive = true;
 
-	st2.name = "Ivan";
-	st2.age = 15;
-	st2.avgMark = 4.5;
-	st2.isAlive = true;
+	temp = st1;
 
-	//string name = st1.avgMark > st2.avgMark ? st1.name : st2.name;
-	Student* t = st1.avgMark > st2.avgMark ? &st1 : &st2;
+	cout << "Before:" << endl;
+	cout << st1.showStudentInfo() << endl;
+	cout << temp.showStudentInfo() << endl;
 
-	cout << "Best student is " << (*t).name << endl;
+	//change(st1);
+	st1.name = "Vova";
+
+	cout << "After:" << endl;
+	cout << st1.showStudentInfo() << endl;
+	cout << temp.showStudentInfo() << endl;
 
 	return 0;
 }
