30da8d2917a032713fa55735159031c607d08ceb
Khaletskiy-Timofey <haltimastro@gmail.com>
2025-04-07 16:49:06 +0300

    Add project files.

---
+++ MainProject/main.cpp
@@ -0,0 +1,23 @@
+#include "Student.h"
+
+int main()
+{
+	Student st1, st2;
+
+	st1.name = "Bogdan";
+	st1.age = 14;
+	st1.avgMark = 9.5;
+	st1.isAlive = true;
+
+	st2.name = "Ivan";
+	st2.age = 15;
+	st2.avgMark = 4.5;
+	st2.isAlive = true;
+
+	//string name = st1.avgMark > st2.avgMark ? st1.name : st2.name;
+	Student* t = st1.avgMark > st2.avgMark ? &st1 : &st2;
+
+	cout << "Best student is " << (*t).name << endl;
+
+	return 0;
+}
