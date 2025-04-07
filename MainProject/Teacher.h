5a895117c1fd525be1ce906387f7852a55dfd484
Khaletskiy-Timofey <haltimastro@gmail.com>
2025-04-07 18:56:11 +0300

    Update business logic

--- MainProject/Teacher.h
+++ MainProject/Teacher.h
@@ -2,21 +2,30 @@
 
 class Teacher
 {
+private:
 	const int BEST_MARK = 9;
 
-public:
-	Student getAllBestStudents(Student* students, int size)
+	int countBestStudents(Student* students, int size)
 	{
+		return 0;
+	}
+
+public:
+	Student* getAllBestStudents(Student* students, int size, int& count)
+	{
+		count = countBestStudents(students, size);
+
+		Student* best_students = new Student[count];
+		int j = 0;
+
 		for (int i = 0; i < size; i++)
 		{
 			if (students[i].avgMark >= BEST_MARK)
 			{
-				return students[i];
+				best_students[j++] = students[i];
 			}
 		}
 
-		Student empty{};
-
-		return empty;
+		return best_students;
 	}
 };
