257ad4f4967719a5c1bf07fe650057e4568990bb
Khaletskiy-Timofey <haltimastro@gmail.com>
2025-04-09 17:57:47 +0300

    Add constructors and destructor

--- MainProject/Student.h
+++ MainProject/Student.h
@@ -8,7 +8,49 @@ public:
 	double avgMark;
 	bool isAlive;
 
-	string showStudentInfo()
+	// Default constructor or constructor without arguments
+	Student()
+	{
+		name = "No name";
+		age = 13;
+		avgMark = 4.0;
+		isAlive = true;
+	}
+
+	// Constructor with argument
+	Student(string input_name)
+	{
+		name = input_name;
+		age = 13;
+		avgMark = 4.0;
+		isAlive = true;
+	}
+
+	// Canonical constructor
+	Student(string input_name, int input_age, double input_avgMark, bool input_isAlive)
+	{
+		name = input_name;
+		age = input_age;
+		avgMark = input_avgMark;
+		isAlive = input_isAlive;
+	}
+
+	// Copy constructor
+	Student(const Student& student)
+	{
+		name = student.name;
+		age = student.age;
+		avgMark = student.avgMark;
+		isAlive = student.isAlive;
+	}
+
+	// Destructor
+	~Student()
+	{
+		cout << "Destructor" << endl;
+	}
+
+	string toString()
 	{
 		string info = "";
 
--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -2,42 +2,19 @@
 
 int main()
 {
-	Student* students = nullptr;
-	int size;
+	Student student1;
+	/*Student student2("Alex");
+	Student student3("Vlad", 13, 10.0, true);
+	Student student4(student3);*/
 
-	cout << "Enter count of students: ";
-	cin >> size;
+	Student student2{ "Alex" };
+	Student student3{ "Vlad", 13, 10.0, true };
+	Student student4{ student3 };
 
-	students = new Student[size];
-
-	cout << "Enter data of students:" << endl;
-	for (int i = 0; i < size; i++)
-	{
-		cout << "Student " << i + 1 << ":" << endl;
-
-		cout << "\tName: ";
-		cin >> students[i].name;
-
-		cout << "\tAge: ";
-		cin >> students[i].age;
-
-		cout << "\tAverage mark: ";
-		cin >> students[i].avgMark;
-
-		cout << "\tIs alive (y/n): ";
-		char answer;
-		cin >> answer;
-
-		students[i].isAlive = islower(answer) == 'y';
-	}
-
-	Teacher teacher;
-
-	Student best_student = teacher.getAllBestStudents(students, size);
-
-	string msg = best_student.isAlive ? "Best student is " + best_student.name : "No best students";
-
-	cout << msg << endl;
+	cout << student1.toString() << endl;
+	cout << student2.toString() << endl;
+	cout << student3.toString() << endl;
+	cout << student4.toString() << endl;
 
 	return 0;
 }
